12929ac2e5931d4dac988b6d998f641293a57920
Sven Seeberg <sseebergelverfeldt@suse.com>
2017-10-26 21:56:58 +0200

    Bump version: 0.4.3 → 1.0.0

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ project = 'git-doccommit'
 year = '2017'
 author = 'Sven Seeberg'
 copyright = '{0}, {1}'.format(year, author)
-version = release = '0.4.3'
+version = release = '1.0.0'
 
 pygments_style = 'trac'
 templates_path = ['.']
