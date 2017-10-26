b20ffd8ff0f7b7a67f7ac81baf37dd0edab6982d
Sven Seeberg <sseebergelverfeldt@suse.com>
2017-10-26 17:31:20 +0200

    Bump version: 0.4.2 → 0.4.3

--- src/doccommit/git.py
+++ src/doccommit/git.py
@@ -238,7 +238,7 @@ class CommitMessage():
         if comments or (self.merge_commits is not None and self.merge_commits != ""):
             result = result + "DocUpdate Merge: " + self.merge_commits
 
-        result = result + "\n~~ created by git-doccommit version 0.4.2"
+        result = result + "\n~~ created by git-doccommit version 0.4.3"
         self.final_message = result
         return no_problem
 
